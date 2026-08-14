16af2207eebbf3ba2f564478c4e7acb487c16935
akina-se <297294322+akina-se@users.noreply.github.com>
2026-08-14 18:57:25 +0900

    test(e2e): fix Playwright localhost vs 127.0.0.1 connection refused error

---
+++ apps/dashboard-e2e/playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  timeout: 60000,
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:4200',
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run start --workspace=dashboard-frontend',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ]
+});
