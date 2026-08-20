8c390b1d9294f7ef175c6b2320bbb10d57e024c3
akina-se <297294322+akina-se@users.noreply.github.com>
2026-08-20 19:58:32 +0900

    ci(frontend): include dashboard-frontend code coverage in CI artifacts and enable karma coverage reporter

---
+++ apps/dashboard-frontend/karma.conf.js
@@ -0,0 +1,38 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // options for Jasmine
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'json-summary', file: 'coverage-summary.json' },
+        { type: 'lcovonly' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: false,
+    singleRun: true
+  });
+};
